d9dedb56b5a15d09c338dcddd28e0f1913dc8a78
Aibek <aibekmukanbetov2@gmail.com>
2023-06-23 17:38:16 +0600

    second commit

--- src/hospital/service/serviceImpl/DoctorServiceImpl.java
+++ src/hospital/service/serviceImpl/DoctorServiceImpl.java
@@ -2,37 +2,49 @@ package hospital.service.serviceImpl;
 
 import hospital.enums.Gender;
 import hospital.model.Doctor;
+import hospital.model.Hospital;
 import hospital.service.DoctorService;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class DoctorServiceImpl implements DoctorService<Doctor> {
 
     private HospitalServiceImpl hospitalService;
-    private List<Doctor> doctors = new ArrayList<>();
     @Override
     public List<Doctor> addDoctor(Doctor doctor) {
-        doctors.add(doctor);
-        return doctors;
+        for (Hospital hospital: hospitalService.getHospitals()){
+            hospital.getDoctors().add(doctor);
+            return hospital.getDoctors();
+        }
+        return null;
     }
 
     @Override
     public Doctor getDoctorById(Long id) {
-        Optional<Doctor> doctor = doctors.stream().filter(x -> x.getId() == id).findFirst();
-        return doctor.orElse(null);
+      for (Hospital hospital: hospitalService.getHospitals()){
+          Optional<Doctor> doctor = hospital.getDoctors().stream().filter(x -> x.getId() == id).findFirst();
+          return doctor.orElse(null);
+      }
+      return null;
     }
 
     @Override
     public List<Doctor> filterByGender(String gender) {
         Gender genderfilter = Gender.valueOf(gender.toUpperCase());
-        List<Doctor> doctors1 = doctors.stream().filter(x -> x.getGender().equals(genderfilter)).toList();
-        return doctors1;
+        for (Hospital hospital: hospitalService.getHospitals()){
+            List<Doctor> doctors = hospital.getDoctors().stream()
+                    .filter(x -> x.getGender().equals(genderfilter)).toList();
+            return doctors;
+        }
+        return null;
     }
 
     @Override
     public List<Doctor> getAllDoctors() {
-        return doctors;
+        for (Hospital hospital: hospitalService.getHospitals()){
+            return hospital.getDoctors();
+        }
+        return null;
     }
 }
